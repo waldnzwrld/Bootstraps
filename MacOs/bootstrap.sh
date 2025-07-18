10b0db267a1ebfc45c1f086df0bdd52334d77567
Walden Bodtker <waldnzwrld@gmail.com>
2025-07-18 01:36:29 -0500

    add a professional brewfile

---
+++ MacOs/bootstrap.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -e
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew bundle
+ssh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
