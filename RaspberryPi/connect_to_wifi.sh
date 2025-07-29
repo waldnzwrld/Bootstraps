80941453b55fde696f7293ccdcf0cca37c502efb
Walden Bodtker <waldnzwrld@gmail.com>
2025-07-29 05:52:31 -0700

    update connect to wifi script

--- RaspberryPi/connect_to_wifi.sh
+++ RaspberryPi/connect_to_wifi.sh
@@ -7,17 +7,21 @@ usage() {
     echo "Usage: $0 -n <name> -p <password>"
     echo "  -n: Set the name"
     echo "  -p: Set the password"
+    echo "  -x: Set this to any value if the network is hidden"
     exit 1
 }
 
 # Parse command line arguments
-while getopts "n:p:h" opt; do
+while getopts "n:p:x:h" opt; do
     case $opt in
         n)
             NETWORK="$OPTARG"
             ;;
         p)
             PASSWORD="$OPTARG"
+            ;;
+        x)
+            HIDDEN="$OPTARG"
             ;;
         h)
             usage
@@ -39,7 +43,14 @@ if [ -z "$NETWORK" ] || [ -z "$PASSWORD" ]; then
     usage
 fi
 
+if [ -z "$HIDDEN" ]; then
+    HIDDEN=""
+else
+    HIDDEN="hidden yes"
+fi
+
+
 echo "Attempting to connect to $NETWORK"
-nmcli dev wifi connect "$NETWORK" password "$PASSWORD"
+nmcli dev wifi connect "$NETWORK" password "$PASSWORD" "$HIDDEN"
 
 
